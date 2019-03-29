e4f02af25926cc666ab429babc83656a2094b600
Hyacinthe04 <mnuriarth28@gmail.com>
2019-03-29 14:54:06 +0200

    import profilr image and comment in admin

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Profile,Image,Comment
 # Register your models here.
+
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal =('likes',)
+
+admin.site.register(Profile)
+admin.site.register(Image)
+admin.site.register(Comments)
+
--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Image,Comments,Likes
 
 # Create your tests here.
+
+class ImageTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.img= Image( image ='image/download_EV7vpwu.jpeg ',image_name = 'land', image_caption ='this is a picture of land',)
